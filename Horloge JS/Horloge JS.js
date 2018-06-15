a67db6b2ffa1bca088862e8a098dc2e455332fb3
LoudThunder <38038952+LoudThunder64@users.noreply.github.com>
2018-06-15 15:51:41 +0200

    Add files via upload

---
+++ Horloge JS/Horloge JS.js
@@ -0,0 +1,19 @@
+(function(){
+
+    //generate clock animations
+    var now       = new Date(),
+        hourDeg   = now.getHours() / 12 * 360 + now.getMinutes() / 60 * 30,
+        minuteDeg = now.getMinutes() / 60 * 360 + now.getSeconds() / 60 * 6,
+        secondDeg = now.getSeconds() / 60 * 360,
+        stylesDeg = [
+            "@keyframes rotate-hour{from{transform:rotate(" + hourDeg + "deg);}to{transform:rotate(" + (hourDeg + 360) + "deg);}}",
+            "@keyframes rotate-minute{from{transform:rotate(" + minuteDeg + "deg);}to{transform:rotate(" + (minuteDeg + 360) + "deg);}}",
+            "@keyframes rotate-second{from{transform:rotate(" + secondDeg + "deg);}to{transform:rotate(" + (secondDeg + 360) + "deg);}}",
+            "@keyframes rotate-hour{from{transform:rotate(" + hourDeg + "deg);}to{transform:rotate(" + (hourDeg + 360) + "deg);}}",
+            "@keyframes rotate-minute{from{transform:rotate(" + minuteDeg + "deg);}to{transform:rotate(" + (minuteDeg + 360) + "deg);}}",
+            "@keyframes rotate-second{from{transform:rotate(" + secondDeg + "deg);}to{transform:rotate(" + (secondDeg + 360) + "deg);}}"
+        ].join("");
+
+    document.getElementById("clock-animations").innerHTML = stylesDeg;
+
+})();
