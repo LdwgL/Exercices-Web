a4ce45a30f2834903b74c633dae61e4327cbe9f3
LoudThunder <38038952+LoudThunder64@users.noreply.github.com>
2018-06-20 10:15:15 +0200

    Add files via upload

--- Canvas JS/init.js
+++ Canvas JS/init.js
@@ -1,21 +1,6 @@
 var can = new MonCanvas(1000, 1000);
-var myfruit = new Fruit(can.ctx, "img/fraise.png", 0,0,500,500);
+var myfruit = new Fruit(can.ctx, "fraise.png", 0,0,500,500);
 myfruit.dessin();
 
 
 var init = new Init();
-
-addEventListener("keydown", function(e) {
-    console.log(this);
-    if (event.defaultPrevented) {
-        return; // Should do nothing if the default action has been cancelled
-    }
-    switch (e.keyCode) {
-        case 39:
-            init.bouge(10);
-            break;
-        case 37:
-            init.bouge(-10);
-            break;
-    }
-})
